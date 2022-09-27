15dd1a5ca7cc8ed4a9a449c7efcc03db74156f4c
Parzival8971 <esh8971@gmail.com>
2022-09-27 21:06:41 +0900

    learnning react

---
+++ src/components/Orders.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Orders = () => {
+  return (
+    <aside>
+      <div className='empty'>
+        <div className='title'>You don't have any orders</div>
+        <div className='subtitle'>Click on a + to add an order</div>
+      </div>
+    </aside>
+  );
+};
+
+export default Orders;
